588ee391c35830f5fa28397a5bd8ecc1e6a151a3
ezraaames <ezradias1110@gmail.com>
2024-02-12 09:09:02 +0530

    hello

---
+++ casetools_ISA1__set8/src/casetools_ISA1__set8/ASciiValue.java
@@ -0,0 +1,14 @@
+package casetools_ISA1__set8;
+
+public class ASciiValue {
+	public static void main(String[] agrs)
+	{
+			
+		char ch='a';
+		int ascii=ch;
+		int castAscii=(int)ch;
+		System.out.println("The ASCII value of "+ch +"is: " +ascii);
+		System.out.println("The ASCII value of "+ch +"is: " +castAscii);
+		}
+
+}
