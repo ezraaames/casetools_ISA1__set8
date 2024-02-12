05566fef77b371753934cd1ed686ffe0baceb666
ezraaames <ezradias1110@gmail.com>
2024-02-12 09:16:45 +0530

    good morning

--- casetools_ISA1__set8/src/casetools_ISA1__set8/ASciiValue.java
+++ casetools_ISA1__set8/src/casetools_ISA1__set8/ASciiValue.java
@@ -10,5 +10,16 @@ public class ASciiValue {
 		System.out.println("The ASCII value of "+ch +"is: " +ascii);
 		System.out.println("The ASCII value of "+ch +"is: " +castAscii);
 		}
+	
+	public class VowelConsonat{
+		public static void main(String[] agrs) {
+			char ch='i';
+			if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u')
+				System.out.println(ch+"is vowel");
+			else
+				System.out.println(ch+ "is consonant");
+		}
+		
+	}
 
 }
